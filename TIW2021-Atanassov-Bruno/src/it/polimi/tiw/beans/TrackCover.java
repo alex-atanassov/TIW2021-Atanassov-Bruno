c7372f380561f981ef825599ba6605c6cfbfbb2b
alex-atanassov <alessandro.atanassov@mail.polimi.it>
2021-05-23 16:14:48 +0200

    TrackCover

---
+++ TIW2021-Atanassov-Bruno/src/it/polimi/tiw/beans/TrackCover.java
@@ -0,0 +1,28 @@
+package it.polimi.tiw.beans;
+
+public class TrackCover {
+	int userid;
+	int id;
+	String title;
+
+	// image
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+}
